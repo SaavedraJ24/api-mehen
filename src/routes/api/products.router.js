943d2f1e4845bc48b4ee6849eacddd8cc5d144fa
SaavedraJ24 <joaquinsaavedraesc@gmail.com>
2025-10-04 13:21:59 -0300

    Rutas REST de products.router con controladores y validaciones

--- src/routes/api/products.router.js
+++ src/routes/api/products.router.js
@@ -1,8 +1,12 @@
-const express = require("express");
+const express = require('express');
+const { validateProductBody } = require('../../middleware/validation');
+const { getAllProducts, getProductById, createProduct, updateProduct, deleteProduct } = require('../../controllers/products.controller');
 const router = express.Router();
-const { getAllProducts, getProductById } = require("../../controllers/products.controller");
 
-router.get("/", getAllProducts);
-router.get("/:id", getProductById);
+router.get('/', getAllProducts);
+router.get('/:id', getProductById);
+router.post('/', validateProductBody, createProduct);
+router.put('/:id', validateProductBody, updateProduct);
+router.delete('/:id', deleteProduct);
 
 module.exports = router;
